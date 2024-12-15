14ab669ac2c0c225f7544249b9b6df71a7b0449f
Olugbenga Taiwo <taiwoolugbenga2018@gmail.com>
2024-12-15 16:42:12 +0100

    fixed daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light", "dark", "valentine", "luxury"],
   },
