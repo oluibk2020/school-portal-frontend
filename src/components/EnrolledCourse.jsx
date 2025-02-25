142a5a16d03b2028ea9fc2db3bab846ae26384b9
Olugbenga Taiwo <taiwoolugbenga2018@gmail.com>
2025-02-25 17:26:47 +0100

    added course assets

--- src/components/EnrolledCourse.jsx
+++ src/components/EnrolledCourse.jsx
@@ -12,6 +12,8 @@ function EnrolledCourse() {
   const { course, isLoading, getOneEnrolledCourse, setIsLoading, setIsOpen } =
     useContext(storeContext);
   const [selectedLessonUrl, setSelectedLessonUrl] = useState("");
+  const [selectedLessonTitle, setSelectedLessonTitle] = useState("");
+  const [selectedLessonDescription, setSelectedLessonDescription] = useState("");
   const [sortedLessons, setSortedLessons] = useState([]);
   const navigate = useNavigate();
   const params = useParams();
@@ -33,6 +35,13 @@ function EnrolledCourse() {
 
       //set selected lesson to the first lesson in the course
       setSelectedLessonUrl(sortedLessons[0].videoUrl);
+
+      //set selected lesson title to the first lesson in the course
+      setSelectedLessonTitle(sortedLessons[0].title);
+
+      //set selected lesson description to the first lesson in the course
+      setSelectedLessonDescription(sortedLessons[0].description);
+
       //set sorted lessons in state
       setSortedLessons(sortedLessons);
     }
@@ -98,20 +107,28 @@ function EnrolledCourse() {
             </div>
             <div className="p-6 sm:p-8">
               <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-white">
-                {course.title}
+                Course - {course.title}
               </h1>
-              <p className="mt-4 text-lg text-center text-gray-600 dark:text-gray-300">
-                {course.description.length > 250 ? (
-                  <>
-                    {course.description.slice(0, 250)}
-                    <span className="text-gray-600 dark:text-gray-300">
-                      ...{" "}
-                    </span>
-                  </>
-                ) : (
-                  course.description
-                )}
-              </p>
+              <div className="my-2 py-4 px-2 border border-gray-400 bg-gray-700 dark:bg-gray-700">
+                <h3 className="text-xl  p-2 font-bold text-center text-white dark:text-white">
+                  Lesson - {selectedLessonTitle}
+                </h3>
+                <p className="mt-1 text-lg text-center text-gray-200 dark:text-gray-200">
+                  {selectedLessonDescription}
+                </p>
+              </div>
+              <a
+                href={course.codeUrl}
+                className="mt-4 inline-block bg-red-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+              >
+                Download Course Codes
+              </a>
+              <a
+                href={course.pdfUrl}
+                className="mt-4 mx-4 inline-block bg-black text-white py-2 px-4 rounded hover:bg-blue-600"
+              >
+                Download Course PDF
+              </a>
               <button
                 type="button"
                 onClick={() => navigate(`/enrolledcourses`)}
@@ -137,6 +154,10 @@ function EnrolledCourse() {
                       onClick={() => {
                         //set selected lesson
                         setSelectedLessonUrl(lesson.videoUrl);
+                        //set selectedLesson Description
+                        setSelectedLessonDescription(lesson.description);
+                        //set selectedLesson Title
+                        setSelectedLessonTitle(lesson.title);
                         //scroll to video player
                         handleScroll("videoPlayer");
                       }}
